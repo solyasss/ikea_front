2244b87a887ec0aa50d42b58b56fe48da103a2c8
olya <olgastet01032005@gmail.com>
2025-04-06 22:01:51 +0300

    Fix Home page: render HeroSection & PromoSection correctly

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,32 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import buyNowImg from "../assets/BuyNow.svg";
-import Headers from "../components/Header/Header";
+
+import HeroSection from "../components/HeroSection/HeroSection";
+import PromoSection from "../components/PromoSection/PromoSection";
+
+import buyNowImg from "../assets/img/icons_svg/BuyNow.svg";
 
 function Home() {
     return (
         <>
-            <Headers />
+            <HeroSection />
+
             <div style={{ padding: "2rem" }}>
-                <h1>Welcome to ByuNow</h1>
+                <h1>Welcome to BuyNow</h1>
 
-                <img src={buyNowImg} alt="logo" style={{ width: 250, height: 100, objectFit: "fill" }} />
-
+                <img
+                    src={buyNowImg}
+                    alt="logo"
+                    style={{ width: 250, height: 100, objectFit: "fill" }}
+                />
 
                 <ul>
                     <li><Link to="/registration">Go to Registration</Link></li>
                     <li><Link to="/products">View Products</Link></li>
                 </ul>
             </div>
+
+            <PromoSection title="ЗНАЙДИ ТЕ, ЩО ШУКАЄШ!" />
         </>
     );
 }
