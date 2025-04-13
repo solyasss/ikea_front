bb03dc0d53bde8c788be1d261eff2fe62fb92a5f
olya <olgastet01032005@gmail.com>
2025-04-13 23:03:13 +0300

    Добавлены кÐfinal realization furniture cards

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,10 +3,12 @@ import { Link } from "react-router-dom";
 
 import HeroSection from "../components/HeroSection/HeroSection";
 import PromoSection from "../components/PromoSection/PromoSection";
-
-import buyNowImg from "../assets/img/icons_svg/BuyNow.svg";
 import CategoryCards from "../components/CategorySlider/CategorySlider"
-import sliderData from "../mocks/sliderData.json"; 
+import FurnitureCards from "../components/FurnitureCards/FurnitureCards";
+import buyNowImg from "../assets/img/icons_svg/BuyNow.svg";
+
+import sliderData from "../mocks/sliderData.json";
+
 
 function Home() { 
 
@@ -28,10 +30,12 @@ function Home() {
                     <li><Link to="/products">View Products</Link></li>
                 </ul>
             </div>
-
+<div className="container">
             <PromoSection title="ЗНАЙДИ ТЕ, ЩО ШУКАЄШ!" />
             <CategoryCards title="РЕКОМЕНДАЦІЇ" cards={sliderData}/>
             <CategoryCards title="НАЙКРАЩІ НАБОРИ" cards={sliderData}/>
+    <FurnitureCards />
+        </div>
         </>
     );
 }
