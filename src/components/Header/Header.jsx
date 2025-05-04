b17df0c24aeffbc46539af09877aebca9c5ae6f5
Sasha <alecsand.unit@gmail.com>
2025-05-04 14:50:14 +0300

    Доработал header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import classes from './Headers.module.css';
+import "./Header.css";
 import burgerIcon from '../../assets/img/header/burger copy.svg';
 import UnionIcon from "../../assets/img/icons_svg/BuyNow.svg";
 import GeoIcon from "../../assets/img/header/Vector.svg";
@@ -6,61 +6,72 @@ import UserIcon from "../../assets/img/header/uil_user1.svg";
 import BuyIcon from "../../assets/img/header/BuyIcon.svg";
 import likeIkon from "../../assets/img/header/Component 1.svg";
 import Search from "../Search/Search";
+import RunningLine from "../RunningLine/RunningLine";
 
 
 export default function Headers() {
     return (
         <>
-            <div className={classes.firstHeader}>
-                <div>
-                    <p className={classes.movingText}>#швидкотазручно</p>
-                    <p className={classes.movingText}>#buy now</p>
-                    <p className={classes.movingText}>#вседлядому</p>
-                    <p className={classes.movingText}>#швидкотазручно</p>
-                    <p className={classes.movingText}>#buy now</p>
-                    <p className={classes.movingText}>#вседлядому</p>
+            <RunningLine />
+            <header className="hedaer__container">
+                <div className="leftMenu">
+                    <div className="burger_container">
+                        <img src={burgerIcon} alt="Меню" width={30} />
+                        <span>Меню</span>
+                    </div>
+
+                    <div>
+                        <img src={UnionIcon} className="logo" width={112} height={36} alt="Лого" />
+                    </div>
+
+                    <nav className="header_menu">
+                        <ul className="menu__list">
+                            <li className="menu__item">
+                                <a href="" className="menu__link">Товари</a>
+                            </li>
+                            <li className="menu__item">
+                                <a href="" className="menu__link">Кімнати</a>
+                            </li>
+                            <li className="menu__item">
+                                <a href="" className="menu__link">Дизайн</a>
+                            </li>
+                        </ul>
+                    </nav>
                 </div>
-                {/* <div>
-                    <p className={classes.movingText}>#швидкотазручно</p>
-                    <p className={classes.movingText}>#buy now</p>
-                    <p className={classes.movingText}>#вседлядому</p>
-                    <p className={classes.movingText}>#швидкотазручно</p>
-                    <p className={classes.movingText}>#buy now</p>
-                    <p className={classes.movingText}>#вседлядому</p>
-                </div> */}
-            </div>
-            <header>
-                <nav className={classes.menu}>
-
-                    <div className={classes.leftMenu}>
-                        <div className={classes.burgerContainer}>
-                            <img src={burgerIcon} alt="Меню" width={30} />
-                            <p>Меню</p>
-                        </div>
-
-                        <img src={UnionIcon} className={classes.logo} width={112} height={36} alt="Лого" />
-
-                        <div className={classes.menuItems}>
-                            <a href="#products"><p>Товари</p></a>
-                            <a href="#rooms"><p>Кімнати</p></a>
-                            <a href="#design"><p>Дизайн</p></a>
-                        </div>
-                    </div>
-
-                    <div className={classes.menuItemsLeft}>
-                        <div className={classes.logoText}>
+                <nav className="header_menu">
+                    <ul className="menu__list">
+                        <li className="menu__item right__menu">
                             <img src={GeoIcon} alt="Гео" />
-                            <a href="#kyiv"><p>Київ</p></a>
-                        </div>
-                        <div className={classes.logoText}>
+                            <a href="" className="menu__link">Київ</a>
+                        </li>
+                        <li className="menu__item right__menu">
                             <img src={UserIcon} alt="Гео" />
-                            <a href="#login"><p>Привіт! Увійдіть в Систему</p></a>
-                        </div>
-                        <div className={classes.logoGroup}>
+                            <a href="" className="menu__link">Привіт! Увійдіть в Систему</a>
+                        </li>
+                        <li className="menu__item right__menu__icons">
+                            <a href="">
+                                <img src={BuyIcon} alt="Гео" />
+                            </a>
+                            <a href="">
+                                <img src={likeIkon} alt="Гео" />
+                            </a>
+                        </li>
+                    </ul>
+                    <li className="menu__item hidden__buttons">
+                        <a href="">
+                            <img src={GeoIcon} alt="Гео" />
+                        </a>
+                        <a href="">
+                            <img src={UserIcon} alt="Гео" />
+                        </a>
+                        <a href="">
                             <img src={BuyIcon} alt="Гео" />
+                        </a>
+                        <a href="">
                             <img src={likeIkon} alt="Гео" />
-                        </div>
-                    </div>
+                        </a>
+                        <img src={burgerIcon} alt="Меню" width={30} />
+                    </li>
                 </nav>
             </header>
             <Search />
---
+++ src/components/RunningLine/RunningLine.jsx
@@ -0,0 +1,36 @@
+import "./RunningLine.css"
+
+function RunningLine(){
+    return(
+        <>
+         <div class="wrap">
+                <div class="items-wrap">
+                    <div class="items marquee">
+                        <div class="item">#вседлядому</div>
+                        <div class="item">#швидкотазручно</div>
+                        <div class="item">#buy now</div>
+                        <div class="item">#вседлядому</div>
+                        <div class="item">#швидкотазручно</div>
+                        <div class="item">#buy now</div>
+                        <div class="item">#вседлядому</div>
+                        <div class="item">#швидкотазручно</div>
+                        <div class="item">#buy now</div>
+                    </div>
+                    <div aria-hidden="true" class="items marquee">
+                        <div class="item">#вседлядому</div>
+                        <div class="item">#швидкотазручно</div>
+                        <div class="item">#buy now</div>
+                        <div class="item">#вседлядому</div>
+                        <div class="item">#швидкотазручно</div>
+                        <div class="item">#buy now</div>
+                        <div class="item">#вседлядому</div>
+                        <div class="item">#швидкотазручно</div>
+                        <div class="item">#buy now</div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default RunningLine;
