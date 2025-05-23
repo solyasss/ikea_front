1ee2b9d02dd709478be4ff646a681a1de8a1c96f
AlexandrS12 <sashasamokish1920@gmail.com>
2025-05-23 22:44:43 +0300

    Доработал страницу кімнати

--- src/components/Room/Room.jsx
+++ src/components/Room/Room.jsx
@@ -11,6 +11,11 @@ import informImg from "../../assets/img/rooms_img/inform1.png"
 import FurnitureCards from "../FurnitureCards/FurnitureCards";
 import ProductsOneSet from "../ProductsOneSet/ProductsOneSet"
 import roomsOneSet from '../../mocks/RoomsOnSet.json';
+import setImg1 from "../../assets/img/rooms_img/set_img1.png";
+import setImg2 from "../../assets/img/rooms_img/set_img2.png"
+import setImg3 from "../../assets/img/rooms_img/set_img3.png"
+
+
 
 
 function Room() {
@@ -45,8 +50,26 @@ function Room() {
                 />
 
             </div>
+            <div className="room-gallery">
+                <ProductsOneSet
+                    title={roomsOneSet.title2}
+                    data={{ images: roomsOneSet.images2 }}
+                />
+            </div>
 
+            <h1>ЗАТИШНІ НАБОРИ</h1>
+            <div className="set-box">
+                <img src={setImg1} />
+                <img src={setImg2} />
+                <img src={setImg3} />
+            </div>
 
+            <h1>ДОДАТКОВО</h1>
+            <div className="additional-information">
+                Lorem ipsum dolor sit amet consectetur. Habitasse consequat egestas lectus blandit consequat. A sed placerat sapien amet scelerisque ullamcorper amet. Elementum consequat risus nullam augue lorem turpis rutrum enim. Sagittis magnis adipiscing non pulvinar ut lacus et montes molestie. Purus vitae risus laoreet vel quis volutpat facilisi justo sed. Sit morbi cras vel non et ut. Ut cursus justo leo nulla ridiculus pharetra egestas libero sed. Pellentesque odio feugiat volutpat amet
+                sed id scelerisque. Ipsum cras vivamus sit ultrices pulvinar. Aliquam donec leo aliquam ornare in enim proin proin eget. Pellentesque tempus eget viverra volutpat nunc hac. Elementum ut ridiculus et eget eu viverra et. Lorem lorem habitant pulvinar sapien cras. Phasellus leo amet purus luctus cursus morbi turpis nibh non. Rhoncus etiam feugiat dolor ac cursus in
+                ac semper. Et urna magna aliquam in sagittis. Eget ultrices ipsum rutrum lacus eu scelerisque sit. Et nibh eget morbi felis fringilla quis habitant. Ipsum aenean integer amet morbi tincidunt. Posuere cras commodo lobortis sit risus dictum nibh. Commodo risus dolor turpis suspendisse convallis.
+            </div>
         </>
     );
 }
