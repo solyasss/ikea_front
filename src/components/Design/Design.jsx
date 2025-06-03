b952a32b48e7c543a30e03cc7bbff9498529221b
AlexandrS12 <sashasamokish1920@gmail.com>
2025-06-03 20:51:52 +0300

    implemented the second part of the page design

--- src/components/Design/Design.jsx
+++ src/components/Design/Design.jsx
@@ -1,5 +1,12 @@
 import inform_img from "../../assets/img/design_hero_section/inform.png"
+import inform2_img from "../../assets/img/design_hero_section/inform2.png"
+import inform3_img from "../../assets/img/design_hero_section/inform3.png"
+
+
 import InformationCard from "../InformationCard/InformationCard"
+import CategorySlider from "../CategorySlider/CategorySlider"
+import sliderData from "../../mocks/sliderData.json";
+
 
 function Design() {
     return (
@@ -7,6 +14,22 @@ function Design() {
             <InformationCard
                 image={inform_img}
                 InternalTitle="ВАЖЛИВА ІНФОРМАЦІЯ"
+                text="Lorem ipsum dolor sit amet consectetur.
+                Bibendum neque at em integer integer. Tempus a mi in enim dui rhoncus vulputate nulla et. 
+                Ornare sed cursus sed viverra."
+            />
+            <CategorySlider title="ВАРІАНТИ ОФОРМЛЕННЯ" cards={sliderData} />
+            <InformationCard
+                image={inform2_img}
+                title="ПОРАДИ"
+                InternalTitle="ІНФОРМАЦІЯ"
+                text="Lorem ipsum dolor sit amet consectetur.
+                Bibendum neque at em integer integer. Tempus a mi in enim dui rhoncus vulputate nulla et. 
+                Ornare sed cursus sed viverra."
+            />
+            <InformationCard
+                image={inform3_img}
+                InternalTitle="ІНФОРМАЦІЯ"
                 text="Lorem ipsum dolor sit amet consectetur.
                 Bibendum neque at em integer integer. Tempus a mi in enim dui rhoncus vulputate nulla et. 
                 Ornare sed cursus sed viverra."
