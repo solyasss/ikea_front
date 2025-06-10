2573d9b1407c56056ff8011055682492c6731f36
AlexandrS12 <sashasamokish1920@gmail.com>
2025-06-10 20:29:32 +0300

    Implemented cards on the design page

--- src/components/Design/Design.jsx
+++ src/components/Design/Design.jsx
@@ -6,11 +6,32 @@ import inform3_img from "../../assets/img/design_hero_section/inform3.png"
 import InformationCard from "../InformationCard/InformationCard"
 import CategorySlider from "../CategorySlider/CategorySlider"
 import sliderData from "../../mocks/sliderData.json";
-
+import "./Design.css"
 
 function Design() {
     return (
         <>
+            <div className="design-card-box">
+                <div className="design-offers-card first-backround-img-card">
+                    <div className="design-card-description">
+                        <h3>ЯК ОБРАТИ СТИЛЬ</h3>
+                        <div>Lorem ipsum dolor sit amet consectetur. Habitasse consequat egestas lectus consequat egestas lectus blandit consequat. A sed  blandit consequat. A sed placerat sapien amet...</div>
+                    </div>
+                </div>
+                <div className="design-offers-card second-backround-img-card">
+                    <div className="design-card-description">
+                        <h3>ЯК ОБРАТИ СТИЛЬ</h3>
+                        <div>Lorem ipsum dolor sit amet consectetur. Habitasse consequat egestas lectus consequat egestas lectus blandit consequat. A sed  blandit consequat. A sed placerat sapien amet...</div>
+                    </div>
+                </div>
+                <div className="design-offers-card third-backround-img-card">
+                    <div className="design-card-description">
+                        <h3>ЯК ОБРАТИ СТИЛЬ</h3>
+                        <div>Lorem ipsum dolor sit amet consectetur. Habitasse consequat egestas lectus consequat egestas lectus blandit consequat. A sed  blandit consequat. A sed placerat sapien amet...</div>
+                    </div>
+                </div>
+            </div>
+
             <InformationCard
                 image={inform_img}
                 InternalTitle="ВАЖЛИВА ІНФОРМАЦІЯ"
