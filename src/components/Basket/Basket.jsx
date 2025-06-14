255061aa6bb59eb200e56cc831f7bf70a6a18cdd
Alexander Shevtsov <alecsandr.unit@gmail.com>
2025-06-15 02:36:48 +0300

    implemented order summary on the cart page

--- src/components/Basket/Basket.jsx
+++ src/components/Basket/Basket.jsx
@@ -1,11 +1,19 @@
 import React, { useEffect, useState } from "react";
 import basket_icon from "../../assets/img/basket/basket.svg";
 import wishlist_icon from "../../assets/img/basket/wishlist.svg";
+import arrow_icon from "../../assets/img/basket/strelka.svg";
+import type_card1 from "../../assets/img/basket/type_card1.svg";
+import type_card2 from "../../assets/img/basket/type_card2.svg";
+import type_card3 from "../../assets/img/basket/type_card3.svg";
+import type_card4 from "../../assets/img/basket/type_card4.svg";
+import type_card5 from "../../assets/img/basket/type_card5.svg";
+import type_card6 from "../../assets/img/basket/type_card6.svg";
+
 import "./Basket.css";
 
 function Basket() {
-    const [card, setCard] = useState(null);
-    const [product, setProduct] = useState(null);
+    const [card, setCard] = useState([]);
+    const [product, setProduct] = useState([]);
 
     useEffect(() => {
         const fetchCards = async () => {
@@ -42,7 +50,6 @@ function Basket() {
 
         fetchProducts();
     }, [card]);
-
 
 
     return (
@@ -89,12 +96,34 @@ function Basket() {
                             </div>
                             <div className="card-extra-info">
                                 <p>{item.price} ₴</p>
+
                             </div>
                         </div>
                     ))}
                 </div>
-                <div className="payment-block">Hello test block</div>
-            </div>
+
+                <div className="payment-block">
+                    <h3>Резюме замовлення</h3>
+                    <div className="sum-details">
+                        <div>Загальна сума:</div>
+                        <div>{card.reduce((sum, item) => sum + item.totalSum, 0)} ₴</div>
+                    </div>
+                    <div className="proceed-payment-block">
+                        <div>Перейти до оформлення замовлення</div>
+                        <img src={arrow_icon} />
+                    </div>
+                    <div className="paymant-cards-box">
+                        <img src={type_card1} />
+                        <img src={type_card2} />
+                        <img src={type_card3} />
+                        <img src={type_card4} />
+                        <img src={type_card5} />
+                        <img src={type_card6} />
+                    </div>
+                    <p>Купуйте зараз, сплачуйте потроху з 0% відсотковою ставкою. Фінансова послуга BCR. <a href="">Дізнайтесь більше тут</a></p>
+                    <a href="">Политика возврата</a>
+                </div>
+            </div >
         </>
     );
 }
