e493c2a43d102d258ac463f76b7584bd5b7f76fe
Alexandr Shevtsov <alecsandr.unit@gmail.com>
2025-06-25 00:07:00 +0300

    Added components to the admin panel

--- src/components/AdminPanel/AdminPanel.jsx
+++ src/components/AdminPanel/AdminPanel.jsx
@@ -1,34 +1,79 @@
 import React, { useState } from "react";
+import ManageСategories from "../ManageСategories/ManageСategories";
+import ManageUsers from "../ManageUsers/ManageUsers";
+import ManageProducts from "../ManageProducts/ManageProducts";
+import ManageCharacteristics from "../ManageProductCharacteristic/ManageProductCharacteristic";
+import ManageProductImages from "../ManageProductImage/ManageProductImage";
 import "./AdminPanel.css";
 
 function AdminPanel() {
     const [activeSection, setActiveSection] = useState("categories");
 
     return (
-        <div className="main-admin-panel">
-            <div className="sidebar">
-                <button onClick={() => setActiveSection("categories")}>
-                    Категории
-                </button>
-                <button onClick={() => setActiveSection("users")}>
-                    Пользователи
-                </button>
-            </div>
-            <div className="main-panel-content">
+        <section className="main-admin-panel">
+            <nav className="admin-menu admin-custom-menu-bg">
+                <ul className="admin-menu-list">
+                    <li
+                        className={activeSection === "purchases" ? "active" : ""}
+                        onClick={() => setActiveSection("categories")}
+                    >
+                        <span className="underline-text">Категории</span>
+                    </li>
+                    <li
+                        className={activeSection === "profile" ? "active" : ""}
+                        onClick={() => setActiveSection("user")}
+                    >
+                        <span className="underline-text">Пользователи</span>
+                    </li>
+                    <li
+                        className={activeSection === "profile" ? "active" : ""}
+                        onClick={() => setActiveSection("products")}
+                    >
+                        <span className="underline-text">Продукты</span>
+                    </li>
+                    <li
+                        className={activeSection === "profile" ? "active" : ""}
+                        onClick={() => setActiveSection("characteristic")}
+                    >
+                        <span className="underline-text">Характеристики</span>
+                    </li>
+                    <li
+                        className={activeSection === "profile" ? "active" : ""}
+                        onClick={() => setActiveSection("image")}
+                    >
+                        <span className="underline-text">Картинки продукта</span>
+                    </li>
+                </ul>
+            </nav>
+            <main className="admin-show-container">
                 {activeSection === "categories" && (
                     <div>
-                        <h2>Категории</h2>
-                        <p>Здесь отображаются категории товаров.</p>
+                        <ManageСategories />
                     </div>
                 )}
-                {activeSection === "users" && (
+                {activeSection === "user" && (
                     <div>
-                        <h2>Пользователи</h2>
-                        <p>Здесь отображаются данные пользователей.</p>
+                        <ManageUsers />
                     </div>
                 )}
-            </div>
-        </div>
+                {activeSection === "products" && (
+                    <div>
+                        <ManageProducts />
+                    </div>
+                )}
+                {activeSection === "characteristic" && (
+                    <div>
+                        <ManageCharacteristics />
+                    </div>
+                )}
+                {activeSection === "image" && (
+                    <div>
+                        <ManageProductImages />
+                    </div>
+                )}
+                {/* test */}
+            </main>
+        </section>
     );
 }
 
